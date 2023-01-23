0f1a26a2a39ff228160e8fec585b5293297f4745
Omkar Sangaonkar <omsangaonkar@gmail.com>
2023-01-23 15:09:42 +0530

    made some chnages for minute digit

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ window.onload = function () {
   // initialize variables for seconds and tens (hundredths of a second)
   let seconds = 00;
   let tens = 00;
+  let minute = 00;
 
   // grab elements from HTML to display timer
   let appendTens = document.getElementById("tens");
   let appendSeconds = document.getElementById("seconds");
+  let appendMinute = document.getElementById("minute");
 
   // grab buttons from HTML to control timer
   let buttonStart = document.getElementById("button-start");
@@ -31,8 +33,10 @@ window.onload = function () {
     clearInterval(Interval);
     tens = "00";
     seconds = "00";
+    minute = "00";
     appendTens.innerHTML = tens;
     appendSeconds.innerHTML = seconds;
+    appendMinute.innerHTML = minute;
   };
 
   // function to start the timer
@@ -61,6 +65,20 @@ window.onload = function () {
     // if seconds is greater than 9, don't add a leading 0
     if (seconds > 9) {
       appendSeconds.innerHTML = seconds;
+    }
+    // if seconds is greater than 59, increment
+    if (seconds > 59) {
+      minute++;
+      appendMinute.innerHTML = "0" + minute;
+      seconds = 0;
+      appendSeconds.innerHTML = "0" + 0;
+      tens = 0;
+      appendTens.innerHTML = "0" + 0;
+    }
+
+    // if seconds is greater than 9, don't add a leading 0
+    if (minute > 9) {
+      appendMinute.innerHTML = minute;
     }
   }
 };
